b566952bd982241d0ff63e10f770c344f0595323
Androssy <89137903+Xenon-Trash@users.noreply.github.com>
2025-06-11 10:32:28 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 -- هذا محمل مبهم أيضا مع العربي
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a9be77bec1dd9acd72e2b8289f71adf7.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4d57f194c893cd55bc64aa34aeffb17e.lua"))()
